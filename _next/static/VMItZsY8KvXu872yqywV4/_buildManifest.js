06dc59976c03958cdca7744848e38711223bb14b
aswathkk <aswathkk@users.noreply.github.com>
2021-09-04 15:09:23 +0000

    deploy: 74663ff7994ec91ea9bf0b9d35a725d355e20030

---
+++ _next/static/VMItZsY8KvXu872yqywV4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84f679ab27a03eec8910.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
